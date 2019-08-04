b4b926755d7ccdc55cd15bc21d73fc7a5de397f5
panda1920 <15102478+panda1920@users.noreply.github.com>
2019-08-05 02:57:10 +0900

    Started implementing my personal game

---
+++ mygame1/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORTNUM = 4000;
+
+app.use((req, res, next) => {
+    console.log("Request has been detected!");
+    // res.send("Hello World!");
+    next();
+});
+app.use(/\/api\/character/, (req, res) => {
+    console.log("You are interacting with character api!");
+    res.json({name: 'joshua', age: 14});
+});
+
+app.listen(PORTNUM, () => {
+    console.log(`Listening on ${PORTNUM}`);
+});
